d5d9fe45ae8ea1fd6d9a4c2ab4e36390f85d49c7
Akshay Sanjeev <asanjeevkeloth@gmail.com>
2025-05-28 09:54:58 +0530

    HW-1-2-submission

---
+++ HW-1-2-FFL/second-attempt/fns.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+def f(u, K, reg, H):
+    if reg == 'act':
+        return (u / K) ** H / (1 + (u / K) ** H)
+    elif reg == 'rep':
+        return 1 / (1 + (u / K) ** H)
+    else:
+        raise ValueError('Regulation unspecified')
+
+def fc(u, Ku, regU, v, Kv, H):
+    if regU == 'act':
+        return (u / Ku) ** H / (1 + (u / Ku) ** H + (v / Kv) ** H)
+    elif regU == 'rep':
+        return 1 / (1 + (u / Ku) ** H + (v / Kv) ** H)
+    else:
+        raise ValueError('Regulation unspecified')
+
+def Gate(gate, u, Ku, regU, v, Kv, regV, H):
+    if gate == 'AND':
+        return f(u, Ku, regU, H) * f(v, Kv, regV, H)
+    elif gate == 'OR':
+        return fc(u, Ku, regU, v, Kv, H) + fc(v, Kv, regV, u, Ku, H)
+    else:
+        raise ValueError('Gate must be AND or OR')
+
+def pulse(t, start, width):
+    return 1.0 if (start <= t <= start + width) else 0.0
+
+def impulse(Si, i):
+    return i if (Si==1) else 0.0
+def sys(t, Q, prm):
+    y, z = Q
+    Sy = pulse(t, prm['ystart'], prm['ywidth'])
+    Sx = pulse(t, prm['start'], prm['width'])
+    x = impulse(Sx, Sx)
+    y = impulse(Sy, y)
+    dydt = prm['By'] + prm['bt_y'] * f(x, prm['Kxy'], prm['regXY'], prm['H']) - prm['alp_y'] * y
+    dzdt = prm['Bz'] + prm['bt_z'] * Gate(prm['gate'], x, prm['Kxz'], prm['regXZ'], y, prm['Kyz'], prm['regYZ'], prm['H']) - prm['alp_z'] * z
+    return [dydt, dzdt]
+
+def SolveAndPlot(prm, axis):
+    t_eval = np.linspace(prm['tmin'], prm['tmax'], prm['N'])
+    Q0 = prm['Q0']
+    sol = solve_ivp(
+        lambda t, Q: sys(t, Q, prm),
+        (prm['tmin'], prm['tmax']),
+        Q0,
+        t_eval=t_eval, method='DOP853'
+    )
+    x = np.zeros_like(t_eval)
+    x = np.where((t_eval >= prm['start']) & (t_eval <= prm['start'] + prm['width']), 1.0, 0.0)
+    axis.plot(t_eval, x, color='black', alpha=.4)
+    axis.plot(sol.t, sol.y[1], label='z(t)')
+    # axis.plot(sol.t, sol.y[0], label='y(t)')
+    axis.set_xlabel('Time($a.u$)')
+    axis.set_ylabel('Z')
+    axis.set_title(prm['header'])
--- HW-1-2-FFL/utilfns.py
+++ HW-1-2-FFL/utilfns.py
@@ -3,28 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
 
-def hill(u, K, H, reg):
-    if reg == 'act' or reg == 1:
-        return (u/K)**H/(1+(u/K)**H)
-    if reg == 'rep' or reg == 0:
-        return 1/(1+(u/K)**H)
-
-def hill2(u, Ku, v, Kv, H, reg):
-    if reg==1 or reg=='act':
-        return (u/Ku)**H/(1+ (u/Ku)**H + (v/Kv)**H)
-    if reg==0 or reg=='rep':
-        return 1/(1+ (u/Ku)**H + (v/Kv)**H)
-
-def Gate(x1, y1, prm):
-    if prm['gate'] == 'AND':
-        AND = hill(x1, prm['Kxz'], prm['H'], prm['regXZ']) * hill(y1, prm['Kyz'], prm['H'], prm['regYZ'])
-        return AND
-    if prm['gate'] == 'OR':
-        OR = hill2(x1, prm['Kxz'], y1, prm['Kyz'], prm['H'], prm['regXZ']) + hill2(y1, prm['Kyz'], x1, prm['Kxz'], prm['H'], prm['regYZ'])
-        return OR
-
-
-def pulse(total_time, pulse_width, dt=.005, start_time=.5):
+def pulse(total_time, pulse_width, dt, start_time):
     t = np.arange(0, total_time, dt)
     pulse = np.zeros_like(t)
     
@@ -35,34 +14,40 @@ def pulse(total_time, pulse_width, dt=.005, start_time=.5):
     pulse[(t >= start_time) & (t < end_time)] = 1
     return t, pulse
 
+def hill(u, K, H, reg):
+    if reg == 'act':
+        return ((u/K)**H)/(1+(u/K)**H)
+    if reg == 'rep':
+        return 1/(1+(u/K)**H)
 
-# def ODEsystem(Q, t):
-#     y, z = Q
-    
-#     xStar2 = np.interp(t, t_grid, xStar)
-#     yStar2 = np.interp(t, t_grid, yStar)
-    
-#     ydot = prm['By'] + prm['Cy'] * hill(xStar2, prm['Kxy'], prm['H'], prm['regX']) - prm['Ay'] * y
-#     zdot = prm['Bz'] + prm['Cz'] * Gate(xStar2, yStar2, prm) - prm['Az'] * z
-    
-#     return [ydot, zdot]
+def hill2(u, Ku, v, Kv, H, reg):
+    if reg=='act':
+        return ((u/Ku)**H)/(1+ (u/Ku)**H + (v/Kv)**H)
+    if reg=='rep':
+        return 1/(1+ (u/Ku)**H + (v/Kv)**H)
+
+def Gate(u, Ku, regu, v, Kv, regv, prm):
+    if prm['gate'] == 'AND':
+        AND = hill(u, Ku, prm['H'], regu) * hill(v, Kv, prm['H'], regv)
+        return AND
+    if prm['gate'] == 'OR':
+        OR = hill2(u, Ku, v, Kv, prm['H'], regu) + hill2(v, Kv, u, Ku, prm['H'], regv)
+        return OR
 
 
-def subplotGenr(prm, Q0, axis, show_pulse=True):  
-    t_grid, yStar = pulse(prm['tTotal'], prm['yWidth'])  # Sy
-    _, xStar = pulse(prm['tTotal'], prm['xWidth'])       # Sx
+def subplotGenr(prm, Q0, axis, show_pulse=True,):  
+    _, yStar = pulse(prm['tTotal'], prm['yWidth'], prm['dt'], prm['start_time'])  # Sy
+    t_grid, xStar = pulse(prm['tTotal'], prm['xWidth'], prm['dt'], prm['start_time'])       # Sx    
     
     def ODEsystem(Q, t):
         y, z = Q
         xStar2 = np.interp(t, t_grid, xStar)
-        yStar2 = np.interp(t, t_grid, yStar)
         ydot = prm['By'] + prm['Cy'] * hill(xStar2, prm['Kxy'], prm['H'], prm['regXY']) - prm['Ay'] * y
-        zdot = prm['Bz'] + prm['Cz'] * Gate(xStar2, yStar2, prm) - prm['Az'] * z
+        zdot = prm['Bz'] + prm['Cz'] * Gate(xStar2, prm['Kxz'], prm['regXZ'], y,prm['Kyz'], prm['regYZ'], prm) - prm['Az'] * z
         return [ydot, zdot]
 
     Qsol = odeint(ODEsystem, Q0, t_grid)
-    axis.plot(t_grid, Qsol[:, 1], '-', markersize=3)
-
+    axis.plot(t_grid, Qsol[:, 1], '-', markersize=1)
     in_pulse = False
     start = 0
     if show_pulse==True:
